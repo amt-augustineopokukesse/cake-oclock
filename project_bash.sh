537c67bae81577becfa74fc7a62a138cd3c59240
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.org>
2022-01-11 10:57:09 +0100

    Remove errors and run tests

--- project_bash.sh
+++ project_bash.sh
@@ -106,3 +106,32 @@ $ npm test
 [phantomjs #0-0]
 
 npm ERR! Test failed.  See above for more details.
+
+#Code test after removing error 
+$ npm test
+
+> cake-bar-js@0.0.0 test /home/ccuser/workspace/why-test-project-2
+> bin/wdio-test
+
+------------------------------------------------------------------
+[phantomjs #0-0] Session ID: 90388c90-72cd-11ec-97aa-e57f7a53fd64
+[phantomjs #0-0] Spec: /home/ccuser/workspace/why-test-project-2/test/features/user-visits-index-test.js
+[phantomjs #0-0] Running: phantomjs
+[phantomjs #0-0]
+[phantomjs #0-0] User visits index
+[phantomjs #0-0]
+[phantomjs #0-0] to post an order
+[phantomjs #0-0]   ✓ starts with a blank order
+[phantomjs #0-0]   ✓ does not provide options outside of working hours
+[phantomjs #0-0]   ✓ displays the selected hour
+[phantomjs #0-0]   ✓ accepts the customer name
+[phantomjs #0-0]   ✓ accepts the cake type
+[phantomjs #0-0]   ✓ accepts multiple fillings
+[phantomjs #0-0]   ✓ accepts the stack size
+[phantomjs #0-0]
+[phantomjs #0-0] to clear an order
+[phantomjs #0-0]   ✓ deletes the selected options
+[phantomjs #0-0]
+[phantomjs #0-0]
+[phantomjs #0-0] 8 passing (5s)
+[phantomjs #0-0]
